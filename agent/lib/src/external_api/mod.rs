645d5c32676aea0f17c52c3f02d344c0b3b4f488
Alexis-Bernard <alexisbernard34310@gmail.com>
2023-02-17 15:53:49 +0100

    feat: add write method & improve read method

--- agent/lib/src/external_api/mod.rs
+++ agent/lib/src/external_api/mod.rs
@@ -1,22 +1,33 @@
 use serialport::{Error, SerialPort};
 
 pub struct ExternalApi {
-    serial: Box<dyn SerialPort>,
+    serial_read_path: String,
+    serial_write_path: String,
+    serial_baud_rate: u32,
 }
 
 impl ExternalApi {
-    pub fn new(path: String, baud_rate: u32) -> Result<Self, Error> {
-        Ok(Self {
-            serial: serialport::new(path, baud_rate).open()?,
-        })
+    pub fn new(serial_read_path: String, serial_write_path: String, serial_baud_rate: u32) -> Self {
+        Self {
+            serial_read_path,
+            serial_write_path,
+            serial_baud_rate,
+        }
     }
 
     pub fn read_from_serial(&mut self) {
+        // Open the serial port
+        let mut serial = serialport::new(&self.serial_read_path, self.serial_baud_rate)
+            .open()
+            .unwrap();
+
+        // Create a vector to hold the data
         let mut data: Vec<u8> = Vec::new();
 
-        let mut buf = [0; 2];
+        // Create a buffer to hold the data
+        let mut buf = [0; 128];
         loop {
-            match self.serial.read(&mut buf) {
+            match serial.read(&mut buf) {
                 Ok(t) => {
                     if t > 0 {
                         println!("Buf {:?}", &buf[..t]);
@@ -44,6 +55,22 @@ impl ExternalApi {
         // Print the data
         println!("Data vector: {:?}", data);
 
-        self.serial.flush().unwrap();
+        serial.flush().unwrap();
+    }
+
+    pub fn write_to_serial(data: &str) {
+        // Open the serial port
+        let mut serial = serialport::new(self.serial_write_path, self.serial_baud_rate)
+            .open()
+            .unwrap();
+
+        // Conver the string to a byte array
+        let buf = data.as_bytes();
+
+        // Write the byte array to the serial port
+        serial.write_all(buf).unwrap();
+
+        // Flush the serial port
+        serial.flush().unwrap();
     }
 }
--- agent/src/main.rs
+++ agent/src/main.rs
@@ -2,7 +2,10 @@ use agent_lib::{external_api::ExternalApi, internal_api::service::InternalApi};
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut external_api = ExternalApi::new("/dev/pts/4".to_string(), 9600)?;
+    let mut external_api =
+        ExternalApi::new("/dev/pts/4".to_string(), "/dev/pts/5".to_string(), 9600);
+
     external_api.read_from_serial();
+
     Ok(())
 }
