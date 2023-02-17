fa6eb60527b4f73eb744e35460db24d9806a8720
iverly <github@iverly.net>
2023-02-17 15:47:59 +0100

    feat: add clap and parse input
    Signed-off-by: iverly <github@iverly.net>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
+use clap::Parser;
 use log::info;
+
+#[derive(Parser)]
+#[clap(
+    version = "0.1",
+    author = "Polytech Montpellier - DevOps",
+    about = "A Serverless runtime in Rust"
+)]
+pub struct LambdoOpts {}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
+    LambdoOpts::parse();
+
     info!("starting up");
 
     // todo: do something
