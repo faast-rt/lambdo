370fd1aed76f41b0bfbcc4bfc2a5b9c606fdae1d
Alexis-Bernard <alexisbernard34310@gmail.com>
2023-02-17 15:29:40 +0100

    feat: define structures
    Signed-off-by: WoodenMaiden <yann.pomie@laposte.net>

--- agent/lib/src/external_api/model.rs
+++ agent/lib/src/external_api/model.rs
@@ -1,16 +1,13 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Debug)]
-pub struct CodeSubmission {
-  pub entrypoint: String,
-  pub code: String,
+pub struct FileModel {
+    fileName: String,
+    fileContent: String,
 }
 
-impl CodeSubmission {
-  pub fn new(entrypoint: String, code: String) -> Self {
-    Self {
-      entrypoint,
-      code,
-    }
-  }
+#[derive(Deserialize, Serialize, Debug)]
+pub struct CodeEntry {
+    files: Vec<FileModel>,
+    script: Vec<String>,
 }
