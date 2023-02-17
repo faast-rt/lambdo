c262a6be629b4d59ba81299f3bb610c2cf9ff55b
iverly <github@iverly.net>
2023-02-17 15:30:33 +0100

    feat: add default logger & anyhow errors
    Signed-off-by: iverly <github@iverly.net>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use log::info;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+    info!("starting up");
+
+    // todo: do something
+
+    info!("shutting down");
+    Ok(())
 }
