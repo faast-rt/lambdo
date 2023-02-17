e46334b2e9dff2a1bb3fce59846f4f54ab8d9658
WoodenMaiden <yann.pomie@laposte.net>
2023-02-17 16:34:33 +0100

    fix: replace unwraps with anyhow errors
    Signed-off-by: WoodenMaiden <yann.pomie@laposte.net>

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -2,15 +2,16 @@ use agent_lib::external_api::ExternalApi;
 use env_logger;
 use log::info;
 use std::error::Error;
+use anyhow::Result;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<()> {
     env_logger::init();
     info!("Starting agent");
 
     let mut external_api =
         ExternalApi::new("/dev/pts/4".to_string(), "/dev/pts/5".to_string(), 9600);
 
-    external_api.read_from_serial();
+    external_api.read_from_serial()?;
 
     info!("Stopping agent");
     Ok(())
