530fa2125efab7378e89fddef34acfa9814e56c8
RedbeanGit <dubois.julien.mail@gmail.com>
2023-04-11 19:47:49 +0200

    feat: add functions to run http request
    + Add httpclient.rs file
    
    Signed-off-by: RedbeanGit <dubois.julien.mail@gmail.com>

---
+++ initramfs/src/httpclient.rs
@@ -0,0 +1,19 @@
+use anyhow::{anyhow, Result};
+use reqwest::{Client, Response};
+
+pub async fn run_get_request(url: &str, token: Option<&str>) -> Result<Response> {
+    let res = match token {
+        Some(token) => Client::new().get(url).bearer_auth(token.clone()),
+        None => Client::new().get(url),
+    }
+    .send()
+    .await
+    .map_err(|e| anyhow!(e).context(format!("Failed to run request to {}", url)))?;
+
+    if !res.status().is_success() {
+        return Err(anyhow!(res.text().await.unwrap_or_default())
+            .context(format!("Failed to get a success response from {}", url)));
+    }
+
+    Ok(res)
+}
