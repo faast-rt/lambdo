ac9452418886513d07fe631d356f4743f1ede40c
RedbeanGit <dubois.julien.mail@gmail.com>
2023-04-11 19:52:06 +0200

    feat: add main entrypoint for initramfs
    + Add main.rs bringing a cli for initramfs
    
    Signed-off-by: RedbeanGit <dubois.julien.mail@gmail.com>

---
+++ initramfs/src/main.rs
@@ -0,0 +1,63 @@
+use anyhow::anyhow;
+use clap::Parser;
+use env_logger::Env;
+use log::{debug, info};
+
+use crate::registry::Registry;
+
+mod httpclient;
+mod image;
+mod registry;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    image: String,
+
+    #[arg(long, default_value = "./init")]
+    init: String,
+
+    #[arg(long, default_value = "./agent")]
+    agent: String,
+
+    #[arg(
+        short,
+        long,
+        default_value = "https://auth.docker.io/token",
+        value_name = "auth"
+    )]
+    auth_url: String,
+
+    #[arg(
+        short,
+        long,
+        default_value = "https://registry-1.docker.io/v2",
+        value_name = "registry"
+    )]
+    registry_url: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Initialize the logger
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
+    // Parse the cli arguments
+    let args = Args::parse();
+    debug!("Running cli with arguments : {:?}", args);
+
+    let registry = Registry::new(&args.registry_url, &args.auth_url);
+
+    info!("Downloading image {}", &args.image);
+    let image = registry.get_image(&args.image).await?;
+    info!("Download done!");
+
+    info!("Writing  to disk ...");
+    image
+        .export_to_initramfs(&args.init, &args.agent)
+        .map_err(|e| anyhow!(e).context("Failed to write filesystem to disk"))?;
+    info!("Writing done!");
+
+    Ok(())
+}
