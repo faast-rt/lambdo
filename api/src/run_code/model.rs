31b8399eb18332bfcead3e49087d0a95ab5b87f1
Alexis Langlet <alexis.langlet@etu.umontpellier.fr>
2023-04-18 08:31:22 +0200

    feat: send mocked instructions to agent
    Signed-off-by: Alexis Langlet <alexis.langlet@etu.umontpellier.fr>

--- api/src/run_code/model.rs
+++ api/src/run_code/model.rs
@@ -1,5 +1,6 @@
 use serde::Deserialize;
 use serde::Serialize;
+use uuid::Uuid;
 
 #[derive(Deserialize)]
 pub struct File {
@@ -33,13 +34,54 @@ pub struct AgentExecution {
 pub struct AgentExecutionData {
     pub id: String,
     pub steps: Vec<AgentExecutionStep>,
+    pub files: Vec<AgentExecutionFile>,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct AgentExecutionStep {
     pub command: String,
-    pub result: Option<u8>,
-    pub stdout: Option<String>,
-    pub stderr: Option<String>,
     pub enable_output: bool,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct AgentExecutionFile {
+    pub filename: String,
+    pub content: String,
+}
+
+impl AgentExecution {
+    pub fn new() -> Self {
+        AgentExecution {
+            r#type: "request".to_string(),
+            code: "run".to_string(),
+            data: AgentExecutionData {
+                id: Uuid::new_v4().to_string(),
+                steps: Vec::new(),
+                files: Vec::new(),
+            },
+        }
+    }
+
+    pub fn add_step(&mut self, step: AgentExecutionStep) {
+        self.data.steps.push(step);
+    }
+
+    pub fn add_file(&mut self, file: AgentExecutionFile) {
+        self.data.files.push(file);
+    }
+}
+
+impl AgentExecutionStep {
+    pub fn new(command: String, enable_output: bool) -> Self {
+        AgentExecutionStep {
+            command,
+            enable_output,
+        }
+    }
+}
+
+impl AgentExecutionFile {
+    pub fn new(filename: String, content: String) -> Self {
+        AgentExecutionFile { filename, content }
+    }
 }
--- api/src/run_code/service.rs
+++ api/src/run_code/service.rs
@@ -1,10 +1,13 @@
 use log::warn;
 use std::env;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Write};
+use std::os::unix::net::UnixStream;
 use std::thread::{spawn, JoinHandle};
 use std::{os::unix::net::UnixListener, path::Path, u32};
 use uuid::Uuid;
 use vmm::VMM;
+
+use crate::run_code::model::{AgentExecution, AgentExecutionStep, AgentExecutionFile};
 
 #[derive(Debug)]
 pub enum Error {
@@ -13,6 +16,8 @@ pub enum Error {
     VmmConfigure(vmm::Error),
 
     VmmRun(vmm::Error),
+
+    BadAgentStatus,
 }
 
 struct VMMOpts {
@@ -29,7 +34,7 @@ struct VMMOpts {
 }
 
 pub fn run_vmm() -> Result<String, Error> {
-    let socket_name = format!("{}.sock", Uuid::new_v4().to_string());
+    let socket_name = format!("/tmp/{}.sock", Uuid::new_v4().to_string());
 
     let socket_path = Path::new(socket_name.as_str());
     if std::fs::metadata(socket_path).is_ok() {
@@ -88,17 +93,72 @@ fn run(opts: VMMOpts) -> Result<(), Error> {
 fn listen(unix_listener: UnixListener) -> JoinHandle<String> {
     let listener_handler = spawn(move || {
         // read from socket
-        let (stream, _) = unix_listener.accept().unwrap();
+        let (mut stream, _) = unix_listener.accept().unwrap();
         let mut response = "".to_string();
 
-        let stream = BufReader::new(stream.try_clone().unwrap());
+        let stream_reader = BufReader::new(stream.try_clone().unwrap());
 
-        for line in stream.lines() {
-            response = format!("{}{}", response, line.unwrap());
+        for line in stream_reader.lines() {
+            let parsed_line = parse_response(line.unwrap(), &mut stream).unwrap();
+            if parsed_line == "" {
+                continue;
+            }
+
+            response = format!("{}{}\n", response, parsed_line);
+            log::trace!("response line: {}", response);
         }
-        println!("response: {}", response);
+        log::debug!("response: {}", response);
 
         response
     });
     listener_handler
+}
+
+fn parse_response(response: String, stream: &mut UnixStream) -> Result<String, Error> {
+    log::trace!("received response from agent: {}", response);
+    if response.contains("\"type\":\"status\"") {
+        // match the status code
+        let status_code = response
+            .split("\"code\":")
+            .nth(1)
+            .unwrap()
+            .split("}")
+            .nth(0)
+            .unwrap()
+            .split("\"")
+            .nth(1)
+            .unwrap();
+        log::debug!("received status code from agent: {}", status_code);
+
+        if status_code == "ready" {
+            send_instructions(stream);
+            Ok("".to_string())
+        } else {
+            Err(Error::BadAgentStatus)
+        }
+    } else {
+        Ok(response)
+    }
+}
+
+fn send_instructions(stream: &mut UnixStream) {
+    // create a new agent execution
+    let mut agent_execution = AgentExecution::new();
+    agent_execution.add_step(AgentExecutionStep {
+        command: "echo 'hello world!'".to_string(),
+        enable_output: true,
+    });
+    agent_execution.add_file(AgentExecutionFile::new("test.txt".to_string(), "hello world!".to_string()));
+
+    let agent_execution_json = serde_json::to_string(&agent_execution).unwrap();
+    let message = format_message(agent_execution_json.as_str());
+    log::debug!("sending agent execution json: {}", message);
+
+    // send the agent execution to the socket
+    let _ = stream.write_all(message.as_bytes()).unwrap();
+}
+
+fn format_message(message: &str) -> String {
+    let message_size = message.len();
+    format!("{:0>8}{}", message_size, message)
 }
