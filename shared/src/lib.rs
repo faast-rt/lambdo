6026136c0995e3eb3362f6be2075533d5349e73c
GridexX <arsene582@gmail.com>
2023-04-18 09:55:23 +0200

    feat(agent): return error message to serial
    Signed-off-by: GridexX <arsene582@gmail.com>

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -32,6 +32,9 @@ pub enum Code {
     /// Agent is ready to communicate
     #[serde(rename = "ready")]
     Ready,
+    // Error during the execution
+    #[serde(rename = "error")]
+    Error,
 }
 
 /// Represents a Status message
@@ -56,6 +59,24 @@ impl StatusMessage {
 impl Default for StatusMessage {
     fn default() -> Self {
         Self::new(Code::Ready)
+    }
+}
+
+/// Represents an Error message
+#[derive(Deserialize, Serialize, Debug)]
+pub struct ErrorMessage {
+    pub r#type: Type,
+    pub code: Code,
+    pub message: String,
+}
+
+impl ErrorMessage {
+    pub fn new(message: String) -> ErrorMessage {
+        ErrorMessage {
+            r#type: Type::Response,
+            code: Code::Error,
+            message,
+        }
     }
 }
 
