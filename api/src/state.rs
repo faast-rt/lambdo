503d206339ac008972b6da4e01282de92814425a
Simon LUCIDO <simon.lucido@etu.umontpellier.fr>
2023-04-22 21:45:44 +0200

    feat(api): rewritten using only gRPC
    Signed-off-by: Simon LUCIDO <simon.lucido@etu.umontpellier.fr>

--- api/src/state.rs
+++ api/src/state.rs
@@ -1,10 +1,10 @@
 use std::fmt::Debug;
 
+use shared::RequestData;
+
 use crate::{
-    config::LambdoConfig,
-    grpc_definitions::lambdo_agent_service_client::LambdoAgentServiceClient,
-    model::{RunRequest, RunResponse},
-    vmm::VMMOpts,
+    config::LambdoConfig, grpc_definitions::lambdo_agent_service_client::LambdoAgentServiceClient,
+    model::RunResponse, vmm::VMMOpts,
 };
 
 pub struct LambdoState {
@@ -16,25 +16,27 @@ pub struct LambdoState {
 pub struct VMState {
     pub id: String,
     pub state: VMStateEnum,
+    pub vm_task: Option<tokio::task::JoinHandle<Result<(), crate::vmm::Error>>>,
     pub vm_opts: VMMOpts,
-    pub request: RunRequest,
+    pub request: RequestData,
     pub response: Option<RunResponse>,
     pub remote_port: Option<u16>,
     pub client: Option<LambdoAgentServiceClient<tonic::transport::Channel>>,
     pub timestamp: std::time::Instant,
-    pub channel: tokio::sync::mpsc::UnboundedSender<()>,
+    pub channel: tokio::sync::mpsc::UnboundedSender<bool>,
 }
 
 impl VMState {
     pub fn new(
         id: String,
         vm_opts: VMMOpts,
-        request: RunRequest,
-        channel: tokio::sync::mpsc::UnboundedSender<()>,
+        request: RequestData,
+        channel: tokio::sync::mpsc::UnboundedSender<bool>,
     ) -> Self {
         VMState {
             id,
             state: VMStateEnum::Waiting,
+            vm_task: None,
             vm_opts,
             request,
             response: None,
