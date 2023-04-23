d0855dbeced68a208b38ad235dd0758420950f08
Simon LUCIDO <simon.lucido@etu.umontpellier.fr>
2023-04-23 21:14:34 +0200

    feat: use gRPC defs as model
    side-effect: no need for a shared crate anymore
    
    Signed-off-by: Simon LUCIDO <simon.lucido@etu.umontpellier.fr>

--- api/build.rs
+++ api/build.rs
@@ -1,6 +1,11 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .out_dir("src/")
+        .message_attribute(".", "#[derive(serde::Deserialize, serde::Serialize)]")
+        .type_attribute(
+            ".grpc_definitions.RegisterResponse.response",
+            "#[derive(serde::Deserialize, serde::Serialize)] #[serde(rename_all = \"snake_case\")]",
+        )
         .compile(&["../shared/proto/lambdo.proto"], &["../shared/proto"])?;
     Ok(())
 }
